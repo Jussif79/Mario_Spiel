1ad8d70af86d3b07f7ebade30ca3816e63af5485
Jussif79 <150897455+Jussif79@users.noreply.github.com>
2023-12-20 20:43:44 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,10 +1,18 @@
 kaboom({
-    global: true,
-    fullscreen: true,
-    scale: 2,
-    debug: true,
-    clearColor: [0, 0, 0, 1],
+  global: true,
+  fullscreen: true,
+  scale: 2,
+  debug: true,
+  clearColor: [0, 0, 0, 1],
 })
+
+const MOVE_SPEED = 120
+const JUMP_FORCE = 360
+const BIG_JUMP_FORCE = 550
+let CURRENT_JUMP_FORCE = JUMP_FORCE
+const ENEMY_SPEED = 20
+
+let isJumping = true
 
 loadRoot('https://i.imgur.com/')
 loadSprite('coin', 'wbKxhcd.png')
@@ -28,132 +36,166 @@ loadSprite('blue-surprise', 'RMqCc1G.png')
 
 
 scene("game", ({ level, score }) => {
-    layers(['bg', 'obj', 'ui'], 'obj')
-  
-    const maps = [
-      [
-        '                                      ',
-        '                                      ',
-        '                                      ',
-        '                                      ',
-        '                                      ',
-        '     %   =*=%=                        ',
-        '                                      ',
-        '                            -+        ',
-        '                    ^   ^   ()        ',
-        '==============================   =====',
-      ],
-    ]
+  layers(['bg', 'obj', 'ui'], 'obj')
 
-    const levelCfg = {
-        width: 20,
-        height: 20,
-        '=': [sprite('block'), solid()],
-        '$': [sprite('coin'), 'coin'],
-        '%': [sprite('surprise'), solid(), 'coin-surprise'],
-        '*': [sprite('surprise'), solid(), 'mushroom-surprise'],
-        '}': [sprite('unboxed'), solid()],
-        '(': [sprite('pipe-bottom-left'), solid(), scale(0.5)],
-        ')': [sprite('pipe-bottom-right'), solid(), scale(0.5)],
-        '-': [sprite('pipe-top-left'), solid(), scale(0.5), 'pipe'],
-        '+': [sprite('pipe-top-right'), solid(), scale(0.5), 'pipe'],
-        '^': [sprite('evil-shroom'), solid(), 'dangerous'],
-        '#': [sprite('mushroom'), solid(), 'mushroom', body()],
-        '!': [sprite('blue-block'), solid(), scale(0.5)],
-        '£': [sprite('blue-brick'), solid(), scale(0.5)],
-        'z': [sprite('blue-evil-shroom'), solid(), scale(0.5), 'dangerous'],
-        '@': [sprite('blue-surprise'), solid(), scale(0.5), 'coin-surprise'],
-        'x': [sprite('blue-steel'), solid(), scale(0.5)],
-    
-      }
-    
-      const gameLevel = addLevel(maps[level], levelCfg)
+  const maps = [
+    [
+      '                                      ',
+      '                                      ',
+      '                                      ',
+      '                                      ',
+      '                                      ',
+      '     %   =*=%=                        ',
+      '                                      ',
+      '                            -+        ',
+      '                    ^   ^   ()        ',
+      '==============================   =====',
+    ],
+  ]
 
-      const scoreLabel = add([
-        text('test'),
-        pos(30,6),
-        layer('ui'),
-        {
-          value: 'test',
-        }
-      ])
+  const levelCfg = {
+    width: 20,
+    height: 20,
+    '=': [sprite('block'), solid()],
+    '$': [sprite('coin'), 'coin'],
+    '%': [sprite('surprise'), solid(), 'coin-surprise'],
+    '*': [sprite('surprise'), solid(), 'mushroom-surprise'],
+    '}': [sprite('unboxed'), solid()],
+    '(': [sprite('pipe-bottom-left'), solid(), scale(0.5)],
+    ')': [sprite('pipe-bottom-right'), solid(), scale(0.5)],
+    '-': [sprite('pipe-top-left'), solid(), scale(0.5), 'pipe'],
+    '+': [sprite('pipe-top-right'), solid(), scale(0.5), 'pipe'],
+    '^': [sprite('evil-shroom'), solid(), 'dangerous'],
+    '#': [sprite('mushroom'), solid(), 'mushroom', body()],
+    '!': [sprite('blue-block'), solid(), scale(0.5)],
+    '£': [sprite('blue-brick'), solid(), scale(0.5)],
+    'z': [sprite('blue-evil-shroom'), solid(), scale(0.5), 'dangerous'],
+    '@': [sprite('blue-surprise'), solid(), scale(0.5), 'coin-surprise'],
+    'x': [sprite('blue-steel'), solid(), scale(0.5)],
 
-      add([text('level ' + 'test', pos(4,6))])
+  }
 
-      function big(){
-        let timer = 0
-        let isBig = false
-        return{
-          update(){
-            if (isBig){
-              timer -= dt()
-              if (timer <= 0){
-                this.smallify
-              }
-            }
-          },
-          isBig(){
-            return isBig
-          },
-          smallify(){
-            this.scale = vec2(1)
-            timer = 0
-            isBig = false
-          },
-          biggify(time){
-            this.scale = vec2(2)
-            timer = time
-            isBig = true
+  const gameLevel = addLevel(maps[level], levelCfg)
+
+  const scoreLabel = add([
+    text('score'),
+    pos(30, 6),
+    layer('ui'),
+    {
+      value: score
+    }
+  ])
+
+  add([text('level ' + 'test', pos(4, 6))])
+
+  function big() {
+    let timer = 0
+    let isBig = false
+    return {
+      update() {
+        if (isBig) {
+          CURRENT_JUMP_FORCE = BIG_JUMP_FORCE
+          timer -= dt()
+          if (timer <= 0) {
+            this.smallify()
           }
         }
+      },
+      isBig() {
+        return isBig
+      },
+      smallify() {
+        this.scale = vec2(1)
+        CURRENT_JUMP_FORCE = JUMP_FORCE
+        timer = 0
+        isBig = false
+      },
+      biggify(time) {
+        this.scale = vec2(2)
+        timer = time
+        isBig = true
       }
+    }
+  }
 
-      const player = add ([
-        sprite('mario'), solid(),
-        pos(30,0),
-        body(),
-        big(),
-        origin('bot')
-      ])
+  const player = add([
+    sprite('mario'), solid(),
+    pos(30, 0),
+    body(),
+    big(),
+    origin('bot')
+  ])
 
-      action('mushroom', (m)=> {
-        m.move(10,0)
-      })
+  action('mushroom', (m) => {
+    m.move(25, 0)
+  })
 
-      player.on("headbump", (obj) => {
-        if (obj.is('coin-surprise')){
-          gameLevel.spawn('$', obj.gridPos.sub(0,1))
-          destroy(obj)
-          gameLevel.spawn('}', obj.gridPos.sub(0,0))
-        }
-        if (obj.is('mushroom-surprise')){
-          gameLevel.spawn('#', obj.gridPos.sub(0,1))
-          destroy(obj)
-          gameLevel.spawn('}', obj.gridPos.sub(0,0))
-        }
-      })
+  player.on("headbump", (obj) => {
+    if (obj.is('coin-surprise')) {
+      gameLevel.spawn('$', obj.gridPos.sub(0, 1))
+      destroy(obj)
+      gameLevel.spawn('}', obj.gridPos.sub(0, 0))
+    }
+    if (obj.is('mushroom-surprise')) {
+      gameLevel.spawn('#', obj.gridPos.sub(0, 1))
+      destroy(obj)
+      gameLevel.spawn('}', obj.gridPos.sub(0, 0))
+    }
+  })
 
-      const MOVE_SPEED = 120
-      const JUMP_FORCE = 360
-    
-      keyDown('left', () => {
-        player.move(-MOVE_SPEED,0)
-      })
+  player.collides('mushroom', (m) => {
+    destroy(m)
+    player.biggify(6)
+  })
 
-      keyDown('right', () => {
-        player.move(MOVE_SPEED,0)
-      })
+  player.collides('coin', (c) => {
+    destroy(c)
+    scoreLabel.value++
+    scoreLabel.text = scoreLabel.value
+  })
 
-      keyPress('space', ()=> {
-        if (player.grounded()){
-          player.jump(JUMP_FORCE)
-        }
-      })
+  action('dangerous', (d) => {
+    d.move(-ENEMY_SPEED, 0)
+  })
+
+  player.collides('dangerous', (d) => {
+    if (isJumping) {
+      destroy(d)
+    } else {
+      go('lose', { score: scoreLabel.value })
+    }
+  })
+
+
+  keyDown('left', () => {
+    player.move(-MOVE_SPEED, 0)
+  })
+
+  keyDown('right', () => {
+    player.move(MOVE_SPEED, 0)
+  })
+
+  player.action(() => {
+    if (player.grounded()){
+      isJumping = false
+    }
+  })
+
+  keyPress('space', () => {
+    if (player.grounded()) {
+      isJumping = true
+      player.jump(CURRENT_JUMP_FORCE)
+    }
+  })
 
 
 })
 
-start("game", {level: 0, score: 0})
+scene('lose', ({ score }) => {
+  add([text(score, 32), origin('center'), pos(width() / 2, height() / 2)])
+})
+
+start("game", { level: 0, score: 0 })
 
 
-// Video stopped at 35:17
+// stopped at min 45:33
